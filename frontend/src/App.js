7f6f09b5cc0c67328a884c4005c3db37dbac4879
Yurii Gromyk <yurii.gromyk@sigmatechnology.com>
2024-01-11 14:58:08 +0100

    Fix routing.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import { Switch, Route } from "react-router-dom";
+import React, {Component} from "react";
+import {Redirect, Route, Switch} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
@@ -10,28 +10,27 @@ import Home from "./components/home.component";
 import Profile from "./components/profile.component";
 
 // import AuthVerify from "./common/auth-verify";
-import { PrivateRoute } from "./components/PrivateRoute";
-import { Redirect } from "react-router-dom";
-import { Container } from "react-bootstrap";
+import {PrivateRoute} from "./components/PrivateRoute";
+import {Container} from "react-bootstrap";
 import Navigation from "./components/Navigation";
 import TodoScreen from "./components/todoscreen";
 
 class App extends Component {
-  render() {
-    return (
-      <Container fluid style={{ paddingLeft: 0, paddingRight: 0 }}>
-        <Navigation />
-        <Switch className="mt-3">
-          <PrivateRoute exact path={["/", "/home"]} component={Home} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/register" component={Register} />
-          <PrivateRoute exact path="/profile" component={Profile} />
-          <PrivateRoute exact path="/todos" component={TodoScreen} />
-          <Redirect from="*" to="/" />
-        </Switch>
-      </Container>
-    );
-  }
+    render() {
+        return (
+            <Container fluid style={{paddingLeft: 0, paddingRight: 0}}>
+                <Navigation/>
+                <Switch>
+                    <PrivateRoute exact path={["/", "/home"]} component={Home} />
+                    <Route exact path="/login" component={Login} />
+                    <Route exact path="/register" component={Register} />
+                    <PrivateRoute exact path="/profile" component={Profile} />
+                    <PrivateRoute exact path="/todos" component={TodoScreen} />
+                    <Redirect from="*" to="/" />
+                </Switch>
+            </Container>
+        );
+    }
 }
 
 export default App;
